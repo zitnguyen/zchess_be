7a1ffd1069a2d785cdf882b0bc222a6aa9fdc971
nntnvlogs <lebaobinh20036@gmail.com>
2026-01-01 22:27:33 +0700

    studentController

---
+++ controllers/studentController.js
@@ -0,0 +1,43 @@
+const Student = require("../models/Student");
+
+exports.getAllStudents = async (req, res) => {
+  try {
+    const students = await Student.find().populate(
+      "parentId",
+      "fullName phone email"
+    );
+    res.json(students);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.createStudent = async (req, res) => {
+  try {
+    const student = new Student(req.body);
+    await student.save();
+    res.status(201).json(student);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+exports.updateStudent = async (req, res) => {
+  try {
+    const student = await Student.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    res.json(student);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+};
+
+exports.deleteStudent = async (req, res) => {
+  try {
+    await Student.findByIdAndDelete(req.params.id);
+    res.json({ message: "Student deleted" });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
